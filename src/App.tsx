9872ce00faf06a1aa9ab5dd9752c50e952ef4393
Pedro Bonfa <pedro.bonfa@estudante.ufjf>
2024-11-22 14:09:20 -0300

    adiciona elementos css como box, titulo e texto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
-
 function App() {
 
   return (
     <>
-      
+      <div>
+        <div className ='box'>
+          <p className='title'>Comprovante de matrícula</p>
+          <div className='boxText'>Matricula: </div>
+          <div className='boxText'>Nome: </div>
+          <div className='boxText'>Curso: </div>
+          <div className='boxText'>Período: </div>
+        </div>
+      </div>
     </>
   )
 }
