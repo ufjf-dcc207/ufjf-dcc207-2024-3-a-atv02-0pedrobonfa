cc05c203d8c7e5c21e1f3d89123c467186eff1f6
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2024-11-28 16:35:52 -0300

    ajusta caixa de matrículas (html e css)

--- src/Matricula.tsx
+++ src/Matricula.tsx
@@ -4,33 +4,10 @@ function Matricula() {
   return (
     <>
         <div className="innerBox">
-            <table id="tabela">
-            <th className='innerTitle'>Mathículas - 2024/3</th>
-                <tr id="TabTitulos">
-                    <td>Disciplina</td>
-                    <td>Nome</td>
-                    <td>Turma</td>
-                    <td>Dia</td>
-                    <td>Hora inicial</td>
-                    <td>Hora final</td>
-                </tr>
-                <tr>
-                    <td>PKM101</td>
-                    <td>Tipos de Pokémon</td>
-                    <td>01A</td>
-                    <td>Segunda-Feira</td>
-                    <td>13:00</td>
-                    <td>16:00</td>
-                </tr>
-                <tr>
-                    <td>PKM102</td>
-                    <td>Montando um time</td>
-                    <td>01A</td>
-                    <td>Terça-Feira</td>
-                    <td>14:00</td>
-                    <td>20:00</td>
-                </tr>
-            </table>
+            <div id="tabela">
+            <div className='innerTitle'>Matrículas - 2024/3</div>
+
+            </div>
         </div>
     </>
   )
